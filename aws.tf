837fb95a29d9a0d8dd2a1595b9044c50324a2bdc
bmp-ntnx <bmpeterson@gmail.com>
2020-08-26 00:56:52 -0600

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -23,7 +23,7 @@ resource "aws_security_group" "instance" {
 
 
 
-resource "aws_instance" "example" {
+resource "aws_instance" "example-cloud" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.instance.id]
